a8ccf49896d243d406a490a840a576ac97b4513d
aejsi5 <felixwolkenhaar@web.de>
2020-12-19 16:26:19 +0100

    Switch to php

--- app/home.php
+++ app/home.php
@@ -12,57 +12,9 @@
 </head>
 
 <body>
-    <div class="navbar-wra">
-        <div class="logo-wrapper">
-            <div class="logo">
-                <img src="../static/img/Banner1.gif" alt="Logo Tierheim Gelnhausen">
-            </div>
-            <div class="oh-div">
-                <img src="../static/img/oh.png" alt="Öffnungszeiten">
-            </div>
-        </div>
-        <div class="main-nav-wrapper">
-            <ul class="main-nav">
-                <li class="mnav-li" id="link-h">
-                    <span class="mnav-span li-active">Home</span>
-                </li>
-                <li class="mnav-li link-zs">
-                    <span class="mnav-span">Zuhause gesucht</span>
-                    <ul class="subnav collapse">
-                        <li class="subnav-li link-zs-h">Hunde</li>
-                        <li class="subnav-li link-zs-k">Katzen</li>
-                        <li class="subnav-li link-zs-a">Andere Tiere</li>
-                        <li class="subnav-li link-zs-w">Weiße Schnauzen</li>
-                        <li class="subnav-li link-zs-v">Vermisste Tiere</li>
-                    </ul>
-                </li>
-                <li class="mnav-li link-zf">
-                    <span class="mnav-span">Zuhause gefunden</span>
-                    <ul class="subnav collapse">
-                        <li class="subnav-li link-demo">Hunde</li>
-                        <li class="subnav-li link-demo">Katzen</li>
-                        <li class="subnav-li link-demo">Andere Tiere</li>
-                        <li class="subnav-li link-demo">Weiße Schnauzen</li>
-                        <li class="subnav-li link-demo">Vermisste Tiere</li>
-                    </ul>
-                </li>
-                <li class="mnav-li link-g">
-                    <span class="mnav-span">Galerie/Archiv</span>
-                    <ul class="subnav collapse">
-                        <li class="subnav-li link-demo">Lorem Ipsum</li>
-                        <li class="subnav-li link-demo">Dolorem Est</li>
-                    </ul>
-                </li>
-                <li class="mnav-li link-k">
-                    <span class="mnav-span">Kontakt/Impressum</span>
-                    <ul class="subnav collapse">
-                        <li class="subnav-li link-demo">Lorem Ipsum</li>
-                        <li class="subnav-li link-demo">Dolorem Est</li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?php
+            include 'navbar.php';
+    ?>
     <main>
         <div class="sidebar">
             <ul class="sidebar">
